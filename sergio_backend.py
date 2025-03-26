9323f492d8176978d764b0da54a32be27886adef
AsherSinghh <asherbasher03@gmail.com>
2025-03-26 17:36:12 -0400

    Fix: use correct credentials filename

--- sergio_backend.py
+++ sergio_backend.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 # Google Sheets setup
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('YOUR_CREDENTIALS_FILE.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
 client = gspread.authorize(creds)
 sheet = client.open("Sergio on time?").sheet1
 
